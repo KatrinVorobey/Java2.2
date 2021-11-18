2cea6ef1eb09eb6ff2f02af3d2dd0a19842f1d5c
KatrinVorobey <90575537+KatrinVorobey@users.noreply.github.com>
2021-11-18 18:25:17 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,13 @@ public class Main {
     public static void main(String[] args) {
         int balance = 100;
         int payment = 1100;
-        int bonus = payment / 100;
+        int bonus;
+        if (payment > 1000)
+            bonus = payment / 100;
+        else
+            bonus = 0;
         int sum = balance + payment + bonus;
         System.out.println("The sum is: " + sum);
         System.out.println("The bonus is: " + bonus);
-
     }
 }
